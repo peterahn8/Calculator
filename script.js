b5e890f34ab9890ed501c332e6a9edd8b1186aaf
Peter Ahn <peterahn8@gmail.com>
2022-12-15 16:54:02 -0500

    Implement operators

--- script.js
+++ script.js
@@ -1,37 +1,76 @@
-function operate(operator, num1, num2) {
-    return operator(num1, num2);
+let currentOperation,
+    storedVal,
+    currentVal;
+let displayedVal = document.querySelector("#display");
+let operators = document.getElementsByClassName("operator");
+
+const input = document.querySelectorAll(".number").forEach(btn => {
+    btn.addEventListener("click", () => {
+        populate(btn.innerHTML);
+        currentVal = parseInt(displayedVal.innerHTML);
+    });
+});
+
+const selectOperator = document.querySelectorAll(".operator").forEach(btn => {
+    btn.addEventListener("click", () => {
+        storedVal = parseInt(displayedVal.innerHTML)
+        currentVal = 0;
+        displayedVal.innerHTML = "0";
+
+        currentOperation = btn.id;
+        let fn = window[currentOperation];
+        if (typeof fn === "function") {
+            currentOperation = fn;
+        }
+    });
+});
+
+const equal = document.querySelector("#equal").addEventListener("click", () => {
+    if (typeof storedVal === "undefined") {
+        displayedVal.innerHTML = "0";
+    } else {
+        displayedVal.innerHTML = operate(currentOperation, currentVal, storedVal);
+        storedVal = parseInt(displayedVal.innerHTML);
+    }
+});
+
+const clear = document.querySelector("#clear").addEventListener("click", () => {
+    displayedVal.innerHTML = "0";
+    currentVal = 0;
+    storedVal = 0;
+});
+
+const del = document.querySelector("#del").addEventListener("click", () => {
+    if (displayedVal.innerHTML.length === 1) {
+        displayedVal.innerHTML = "0";
+    } else {
+        displayedVal.innerHTML = displayedVal.innerHTML.slice(0,-1);
+    }
+    currentVal = parseInt(displayedVal.innerHTML);
+});
+
+function populate(val) {
+    if (displayedVal.innerHTML === "0") {displayedVal.innerHTML = "";}
+    displayedVal.innerHTML += val;
 }
 
-function add(a, b) {
-    return a + b;
-};
+function operate(operator, a, b) {while (operator) {return operator(a, b)};}
 
-function subtract (a, b) {
-    return a - b;
-};
+function add(a, b) {return a + b;}
 
-function sum(array) {
-    return array.reduce((a, b) => a + b, 0);
-}
+function subtract (a, b) {return b - a;}
 
-function multiply(array) {
-    return array.reduce((a,b) => a * b, 1);
-}
+function multiply(a, b) {return a * b;}
 
-function power(a, b) {
-    return Math.pow(a, b);
-}
+function power(a, b) {return Math.pow(b, a);}
 
 function factorial(a) {
     let factArray = [a];
-
     if (a === 0) {
         return 1;
     };
-
     for (let i = 1; i < a; i++) {
         factArray.push(i);
     };
-
     return factArray.reduce((a, b) => a * b);
-};
+}
